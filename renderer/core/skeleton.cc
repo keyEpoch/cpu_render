1331b428058b9530d2fe4d3bff113a22652efac8
zhanghuan <1801210747@pku.edu.cn>
2020-06-22 15:46:51 +0800

    add get_scale

--- renderer/core/skeleton.cc
+++ renderer/core/skeleton.cc
@@ -60,6 +60,34 @@ static quat_t get_rotation(joint_t* joint, float frame_time) {
     }
 }
 
+static vec3_t get_scale(joint_t *joint, float frame_time) {
+    int num_scales = joint->num_scales;
+    float *scale_times = joint->scale_times;
+    vec3_t *scale_values = joint->scale_values;
+
+    if (num_scales == 0) {
+        return vec3_new(1, 1, 1);
+    } else if (frame_time <= scale_times[0]) {
+        return scale_values[0];
+    } else if (frame_time >= scale_times[num_scales - 1]) {
+        return scale_values[num_scales - 1];
+    } else {
+        int i;
+        for (i = 0; i < num_scales - 1; i++) {
+            float curr_time = scale_times[i];
+            float next_time = scale_times[i + 1];
+            if (frame_time >= curr_time && frame_time < next_time) {
+                float t = (frame_time - curr_time) / (next_time - curr_time);
+                vec3_t curr_scale = scale_values[i];
+                vec3_t next_scale = scale_values[i + 1];
+                return vec3_lerp(curr_scale, next_scale, t);
+            }
+        }
+        assert(0);
+        return vec3_new(1, 1, 1);
+    }
+}
+
 void skeleton_update_joints(skeleton_t* skeleton, float frame_time) {
     // fmod 相当于是对 float 的 mod，也就是取余
     frame_time = (float)fmod(frame_time, skeleton->max_time);   
@@ -68,7 +96,10 @@ void skeleton_update_joints(skeleton_t* skeleton, float frame_time) {
             joint_t* joint = &skeleton->joints[i];
             vec3_t translation = get_translation(joint, frame_time);
             quat_t rotation = get_rotation(joint, frame_time);
-            
+            vec3_t scale = get_scale(joint, frame_time);
+
+            mat4_t joint_matrix;
+            mat4_t normal_matrix;
         }
     }
 }
